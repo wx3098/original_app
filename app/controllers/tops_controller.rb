d7f2457b1fa0c5b0df3f187929285daec347b494
kouichi shinkokura <stndard809@gmail.com>
2023-02-19 15:27:18 +0900

    試行錯誤

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -3,15 +3,19 @@ class TopsController < ApplicationController
   def index
 
     # @hospitals = current_hospital.order(created_at: "DESC")
-    
+  
+      
     if params[:hospital]&&params[:hospital][:name].present?
+      # @hospitals = Hospital.hospital(hospital_params).includes(:medical_departments)
       @hospitals = Hospital.where('name LIKE ?', "%#{params[:hospital][:name]}%")
-      @hospitals = Hospital.all.includes(:medical_departments)
+      @users = User.includes(medical_departments: :hospital)
+      
+      # @hospitals = MedicalDepartment.includes(:hospital).where(hospitals: {id: current_hospital.id} )
+      
       # @departments = MedicalDepartment.includes(:hospitals).where('name LIKE ?', "%#{params[:hospital][:name]}%")
       # @departments = MedicalDepartment.all.includes(:hospital)
       # @departments = MedicalDepartment.reflect_on_all_associations.map(:name)
-      
-    else
+    else 
       @hospitals = Hospital.all
     end
   end
