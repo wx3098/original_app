2b98958fd025e6103596925401267e50a1f7c136
kouichi shinkokura <stndard809@gmail.com>
2023-03-14 21:32:06 +0900

    3/14テストコード

---
+++ spec/system/favorite_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'お気に入り登録', type: :system do
+let!(:hospital1){FactoryBot.create(:hospital1)}
+let!(:secound_user){FactoryBot.create(:secound_user)}
+
+  context 'ユーザーが病院を検索した場合' do
+    it '病院をお気に入りに登録出来る' do
+      visit  new_hospital_session_path
+      fill_in 'hospital[email]', with: hospital1.email
+      fill_in 'hospital[password]', with: hospital1.password
+      click_on 'ログイン'
+      visit medical_departments_path
+      click_on '病院用ログアウト'
+      visit home_path
+      click_on 'ユーザーログイン'
+      fill_in 'user[email]', with: secound_user.email
+      fill_in 'user[password]', with: secound_user.password
+      click_on 'ログイン'
+      visit tops_path
+      fill_in 'hospital[name]', with: hospital1.name
+      click_on '検索'
+      click_on 'お気に入り'
+      sleep(5)
+      expect(page).to have_content 'お気に入り解除'
+    end
+  end
+end
--- spec/system/hospital_spec.rb
+++ spec/system/hospital_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe '病院ログイン', type: :system do
   let!(:hospital1){FactoryBot.create(:hospital1)}
+  let!(:secound_user){FactoryBot.create(:secound_user)}
+
   describe '新規登録' do
-    context '病院' do
+    context '病院側の' do
       it '病院の新規登録ができること' do
         visit  new_hospital_registration_path
-        fill_in 'hospital[name]', with: '古谷総合病院'
-        fill_in 'hospital[email]', with: 'furu@ya.com'
-        fill_in 'hospital[password]', with: 'furu@ya.com'
-        fill_in 'hospital[password_confirmation]', with: 'furu@ya.com'
+        fill_in 'hospital[name]', with: hospital1.name
+        fill_in 'hospital[email]', with: hospital1.email
+        fill_in 'hospital[password]', with: hospital1.password
+        fill_in 'hospital[password_confirmation]', with: hospital1.password_confirmation
         click_on '登録'
-        visit new_hospitals_medical_department_path
-        expect(page).to have_content '診療科'
+        visit medical_departments_path
       end
     end
   end
@@ -22,40 +23,66 @@ RSpec.describe '病院ログイン', type: :system do
       fill_in 'hospital[email]', with: hospital1.email
       fill_in 'hospital[password]', with: hospital1.password
       click_on 'ログイン'   
-      visit new_hospitals_medical_department_path
-      expect(page).to have_content '診療科'
+      visit medical_departments_path
     end
   end
-
-
-
   
-    context '病院からログインし' do
+    context '予約済みの患者さんがいる場合' do
       it '診療科と時間を設定をする' do
         visit  new_hospital_session_path
         fill_in 'hospital[email]', with: hospital1.email
         fill_in 'hospital[password]', with: hospital1.password
         click_on 'ログイン'
+        visit medical_departments_path
+        click_on '新規診療科作成'
         visit new_hospitals_medical_department_path
+        select '5分', from: 'medical_department[wait_time]'
         fill_in 'medical_department[name]', with:'小児科'
-        select '5分', from: 'wait_time'
         click_on '登録する'
-        expect(page).to have_content '診療科一覧'
+        visit medical_departments_path
       end
     end
-    context '病院からログインし' do
-      it '時間に関係なく呼び出しが出来る' do
+
+    context '予約済みの患者さんがいる場合' do
+      it '診療科から呼び出しが出来る' do
+        #病院の登録、診療科の登録を行う。
         visit  new_hospital_session_path
         fill_in 'hospital[email]', with: hospital1.email
         fill_in 'hospital[password]', with: hospital1.password
         click_on 'ログイン'
+        visit medical_departments_path
+        click_on '新規診療科作成'
         visit new_hospitals_medical_department_path
+        select '5分', from: 'medical_department[wait_time]'
         fill_in 'medical_department[name]', with:'小児科'
-        select '5分', from: 'wait_time'
         click_on '登録する'
         visit medical_departments_path
-        click_link '呼び出しボタン'
-        expect(page).to have_content '診療科情報'
+        click_on '病院用ログアウト'
+
+        # ユーザー側で受付を行う。
+        visit home_path
+        click_on 'ユーザーログイン'
+        fill_in 'user[email]', with: secound_user.email
+        fill_in 'user[password]', with: secound_user.password
+        click_on 'ログイン'
+        visit tops_path
+        fill_in 'hospital[name]', with: hospital1.name
+        click_on '検索'
+        click_on '高橋病院'
+        click_on '小児科'
+        click_on '受付'
+        visit mains_path
+        click_on 'ユーザー用ログアウト'
+
+        #病院側に入り呼び出しボタンを押す
+        visit  new_hospital_session_path
+        fill_in 'hospital[email]', with: hospital1.email
+        fill_in 'hospital[password]', with: hospital1.password
+        click_on 'ログイン'
+        visit medical_departments_path
+        click_on '呼び出しボタン'
+        click_on '呼び出し'
+        visit medical_departments_path
       end
     end
   end
@@ -64,3 +91,4 @@ RSpec.describe '病院ログイン', type: :system do
 
 
 
+ 
