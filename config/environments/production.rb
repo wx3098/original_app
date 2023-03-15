2ef1f0c5e6d8c5b4a31c5ffe92dadabb05d9eab4
kouichi shinkokura <stndard809@gmail.com>
2023-03-15 20:01:41 +0900

    herokuでletter_opener

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'https://boiling-refuge-98520.herokuapp.com'}
+  config.action_mailer.delivery_method = :letter_opener_web
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.force_ssl = true
--- spec/models/medical_department_spec.rb
+++ spec/models/medical_department_spec.rb
@@ -13,5 +13,23 @@ RSpec.describe MedicalDepartment, type: :model do
         expect(department ).not_to be_valid
       end
     end
+    context '同じ病院内で診療科名が重複する場合' do
+      it 'バリデーションにひっかる' do
+        hospital = Hospital.create(name: '病院A')
+        department1 = MedicalDepartment.create(name: '小児科', hospital: hospital)
+        department2 = MedicalDepartment.new(name: '小児科', hospital: hospital)
+        expect(department2).not_to be_valid
+      end
+    end
+
+    context '別の病院で同じ診療科名を使用する場合' do
+      it 'バリデーションにひっかからない' do
+        hospital1 = Hospital.create(name: '病院A')
+        hospital2 = Hospital.create(name: '病院B')
+        department1 = MedicalDepartment.create(name: '皮膚科', hospital: hospital1)
+        department2 = MedicalDepartment.create(name: '皮膚科', hospital: hospital2)
+        expect(department2.valid?).to eq(true)
+      end
+    end
   end
 end
