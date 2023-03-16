72e93833752980a66a77fd75ef8cb5ed7193ef77
kouichi shinkokura <stndard809@gmail.com>
2023-03-16 15:41:42 +0900

    3/16修正

--- app/mailers/medical_appointment_mailer.rb
+++ app/mailers/medical_appointment_mailer.rb
@@ -3,6 +3,7 @@ class MedicalAppointmentMailer < ApplicationMailer
     @appointment = appointment
     @user = @appointment.user
     @medical_department = @appointment.medical_department
+    @hospital = @medical_department.hospital
 
     mail to: @user.email, subject: "診察室へお入り下さい" 
   end
