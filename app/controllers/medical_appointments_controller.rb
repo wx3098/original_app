b6f8052a2c12e306eea410c8744dfd8df9f8b0af
kouichi shinkokura <stndard809@gmail.com>
2023-03-24 01:31:59 +0900

    lineメッセージ14

--- app/controllers/medical_appointments_controller.rb
+++ app/controllers/medical_appointments_controller.rb
@@ -10,67 +10,70 @@ class MedicalAppointmentsController < ApplicationController
     end
   end
 
+  # def destroy
+  #   @appointment = MedicalAppointment.find_by(id: params[:id])
+  
+  #   unless @appointment.nil?
+  #     if user_signed_in?
+  #       if current_user.provider == "line"
+  #         @user = @appointment.user
+  #         @medical_department = @appointment.medical_department
+  #         @hospital = @medical_department.hospital
+         
+  #         client = Line::Bot::Client.new do |config|
+  #           config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+  #           config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+  #         end
+  
+  #         message = {
+  #           type: 'text',
+  #           text: "#{@appointment.user.name}様 #{@medical_department.hospital.name} #{@medical_department.name} お待たせしました。診察室へお入り下さい。"
+  #         }
+  
+  #         response = client.push_message(@user.uid, message)
+  #       elsif current_user.provider == "email"
+  #         MedicalAppointmentMailer.send_notification(@appointment).deliver_now
+  #       end
+  #     end
+      
+  #     @appointment.destroy
+  #     flash[:notice] = '呼び出しました'
+  #   end
+  
+  #   redirect_to medical_departments_path
+  # 
+  
   def destroy
     @appointment = MedicalAppointment.find_by(id: params[:id])
   
-    unless @appointment.nil?
-      if user_signed_in?
-        if current_user.provider == "line"
-          @user = @appointment.user
-          @medical_department = @appointment.medical_department
-          @hospital = @medical_department.hospital
-         
-          client = Line::Bot::Client.new do |config|
-            config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-            config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-          end
+    if user_signed_in?
+      if current_user.email.present?
+        # メールアドレスが登録されている場合
+        MedicalAppointmentMailer.send_notification(@appointment).deliver_now
+        flash[:notice] = '呼び出しました'
+      else
+        # メールアドレスが登録されていない場合
+        @user = @appointment.user
+        @medical_department = @appointment.medical_department
+        @hospital = @medical_department.hospital
   
-          message = {
-            type: 'text',
-            text: "#{@appointment.user.name}様 #{@medical_department.hospital.name} #{@medical_department.name} お待たせしました。診察室へお入り下さい。"
-          }
-  
-          response = client.push_message(@user.uid, message)
-        elsif current_user.provider == "email"
-          MedicalAppointmentMailer.send_notification(@appointment).deliver_now
+        client = Line::Bot::Client.new do |config|
+          config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+          config.channel_token = ENV['LINE_CHANNEL_TOKEN']
         end
+  
+        message = {
+          type: 'text',
+          text: "#{appointment.user.name}様 #{medical_department.hospital.name} #{medical_department.name} お待たせしました。診察室へお入り下さい。"
+        }
+  
+        response = client.push_message(@user.uid, message)
+        flash[:notice] = '呼び出しました'
       end
-      
-      @appointment.destroy
-      flash[:notice] = '呼び出しました'
     end
   
-    redirect_to medical_departments_path
+    redirect_to medical_appointments_path
   end
-  
-  # def call_patient
-  #   @appointment = MedicalAppointment.find_by(id: params[:id])
-  
-  #   if user_signed_in?
-  #     if current_user.provider == "line"
-  #       @user = @appointment.user
-  #       @medical_department = @appointment.medical_department
-  #       @hospital = @medical_department.hospital
-  
-  #       client = Line::Bot::Client.new do |config|
-  #         config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-  #         config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-  #       end
-  
-  #       message = {
-  #         type: 'text',
-  #         text: "#{@user} #{@hospital} #{@medical_department} 呼び出し中です。"
-  #       }
-  
-  #       response = client.push_message(current_user.uid, message)
-  #     elsif current_user.provider == "email"
-  #       MedicalAppointmentMailer.send_call_notification(@appointment).deliver_now
-  #     end
-  #   end
-  
-  #   flash[:notice] = '呼び出しました'
-  #   redirect_to medical_departments_path
-  # end
   
 end
 
