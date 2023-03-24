13a7df866f3ac246ff81e6c8b74192d9cac29057
kouichi shinkokura <stndard809@gmail.com>
2023-03-24 09:26:50 +0900

    lineメッセージ21

--- app/controllers/medical_appointments_controller.rb
+++ app/controllers/medical_appointments_controller.rb
@@ -42,31 +42,28 @@ class MedicalAppointmentsController < ApplicationController
   
   #   redirect_to medical_departments_path
   # end
-
   def destroy
     @appointment = MedicalAppointment.find_by(id: params[:id])
     
     unless @appointment.nil?
-      if user_signed_in?
-        if current_user.provider == "line"
-          @user = @appointment.user
-          @medical_department = @appointment.medical_department
-          @hospital = @medical_department.hospital
-           
-          client = Line::Bot::Client.new do |config|
-            config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-            config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-          end
-    
-          message = {
-            type: 'text',
-            text: "#{current_user.name}様、診察室へお入り下さい。"
-          }
-    
-          response = client.push_message(@user.uid, message)
-        else
-          MedicalAppointmentMailer.send_notification(@appointment).deliver_now
+      if user_signed_in? && current_user.provider == "line"
+        @user = @appointment.user
+        @medical_department = @appointment.medical_department
+        @hospital = @medical_department.hospital
+          
+        client = Line::Bot::Client.new do |config|
+          config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+          config.channel_token = ENV['LINE_CHANNEL_TOKEN']
         end
+  
+        message = {
+          type: 'text',
+          text: "#{current_user.name}様、診察室へお入り下さい。"
+        }
+  
+        response = client.push_message(@user.uid, message)
+      else
+        MedicalAppointmentMailer.send_notification(@appointment).deliver_now
       end
       
       @appointment.destroy
@@ -74,6 +71,6 @@ class MedicalAppointmentsController < ApplicationController
     end
     
     redirect_to medical_departments_path
-  end
+  end  
 end
 
