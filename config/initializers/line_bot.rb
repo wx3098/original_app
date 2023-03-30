24884a78e0e5ada3738813aa2f452617aba9dc70
kouichi shinkokura <stndard809@gmail.com>
2023-03-30 13:40:13 +0900

    lineメッセージ30

--- config/initializers/line_bot.rb
+++
@@ -1,6 +0,0 @@
-require 'line/bot'
-
-Rails.application.config.middleware.use Line::Bot::Middleware do
-  config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-  config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-end
